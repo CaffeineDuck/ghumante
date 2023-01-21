d17531f93b2a1fbec0a34e326d3f9512eff4efa8
Mahesh C. Regmi <0xmahadev@gmail.com>
2023-01-21 15:41:11 +0545

    add user verification for client

--- backend_/firebase/client.py
+++ backend_/firebase/client.py
@@ -1,25 +1,60 @@
 import typing as ty
+from firebase_admin import auth
 from firebase_admin.auth import verify_id_token  # type: ignore
 from pathlib import Path
-from firebase_admin import App, initialize_app, credentials
+from firebase_admin import initialize_app, credentials
+
+from firebase_admin.firestore import client as firestore_client
 
 import logging
 
 
-class FirebaseClient:
-    def __init__(self, config_file="private_key.json", logger=logging.Logger(__name__)):
+class FirebaseUser(ty.TypedDict):
+    phone_number: str
+    uid: str
 
-        config_full_path = Path(__file__).parent / config_file
-        self.creds = credentials.Certificate(config_full_path)
+
+class FirebaseClient:
+
+    default_config_file = Path(__file__).parent / "private_key.json"
+
+    def __init__(
+        self,
+        config_file=default_config_file,
+        logger=logging.Logger(__name__),
+    ):
 
         self._logger = logger
 
-        self._app = initialize_app(self.creds)
+        creds = credentials.Certificate(config_file)
+        self._app = initialize_app(creds)
 
-    def verify_id_token(self, token: str) -> ty.Optional[dict]:
+        self._logger.info("Firebase Client initialized.")
+
+        self._db = firestore_client(app=self._app)
+
+        self._logger.info("Firestore Client initialized.")
+
+    def verify_id_token(self, token: str) -> ty.Optional[FirebaseUser]:
         try:
             return verify_id_token(id_token=token, app=self._app)
 
         except Exception as e:
             self._logger.error(f"Error verifying token: {e}")
             return
+
+    def get_all_users(self):
+        users = auth.list_users(app=self._app).iterate_all()
+
+        results: list[FirebaseUser] = []
+
+        for user in users:
+            result = FirebaseUser(phone_number=user.phone_number, uid=user.uid)
+            results.append(result)
+
+        return results
+
+    def get_user_by_uid(self, uid: str):
+
+        user = auth.get_user_by_uid(uid=uid, app=self._app)
+        return FirebaseUser(phone_number=user.phone_number, uid=user.uid)
