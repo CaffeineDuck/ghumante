61cb49e612489b8a306fce4c4eca775f6b665a79
Neeraj Lamsal <itskop520@gmail.com>
2023-01-21 17:14:15 +0545

    add swagger docs

--- backend_/backend_/urls.py
+++ backend_/backend_/urls.py
@@ -18,8 +18,23 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("core.urls")),
+    path(
+        "docs/",
+        TemplateView.as_view(
+            template_name="swagger-ui.html",
+            extra_context={"schema_url": "api_schema"},
+        ),
+        name="swagger-ui",
+    ),
+    path(
+        "api_schema/",
+        get_schema_view(title="API Schema", description="Guide for the REST API"),
+        name="api_schema",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
