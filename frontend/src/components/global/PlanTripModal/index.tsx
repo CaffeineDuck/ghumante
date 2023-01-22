c4f585f1aefcec7e379dd85f981dadbe2e626063
Niraj Pradhan <niraj.pradhan.393@gmail.com>
2023-01-22 22:54:52 +0545

    fix: conflicts

--- frontend/src/components/global/PlanTripModal/index.tsx
+++ frontend/src/components/global/PlanTripModal/index.tsx
@@ -32,6 +32,7 @@ import useCustomToast from "@/hooks/useCustomToast";
 //import useCurrentStep from "@/hooks/useCurrentStep";
 import { useTripContext } from "@/context/TripContext";
 import { ColorChangingCircle } from "./ColorChangingCircle";
+import PhoneVerification from "../PhoneVerification";
 interface PlanTripModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -39,7 +40,7 @@ interface PlanTripModalProps {
 
 const PlanTripModal: React.FC<PlanTripModalProps> = ({ isOpen, onClose }) => {
   const toast = useCustomToast();
-
+  const { isAuthenticated } = useContext(AppContext);
   const {
     steps,
     occupiedHours,
@@ -52,7 +53,11 @@ const PlanTripModal: React.FC<PlanTripModalProps> = ({ isOpen, onClose }) => {
   } = useTripContext();
 
   return (
-    <Modal size="5xl" isOpen={isOpen} onClose={onClose}>
+    <Modal
+      size={isAuthenticated ? "5xl" : "2xl"}
+      isOpen={isOpen}
+      onClose={onClose}
+    >
       <ModalOverlay />
       <ModalContent>
         <ModalHeader></ModalHeader>
@@ -67,97 +72,120 @@ const PlanTripModal: React.FC<PlanTripModalProps> = ({ isOpen, onClose }) => {
           py="2rem"
           minH="30rem"
         >
-          <Flex gap={{ base: "1.5rem" }} justify="center">
-            {currentStep.showSidebar && (
-              <VStack
-                px="4"
-                w="14rem"
-                align="flex-start"
-                divider={
-                  <Box
-                    h="2.25rem"
-                    w="1.42px"
-                    bg="gray.500"
-                    position="relative"
-                    left="15px"
-                  />
-                }
-              >
-                {arrivalDateTime && (
-                  <Flex align="center" gap={{ base: "1.5rem", md: "1.82rem" }}>
-                    <ColorChangingCircle
-                      currentStepNumber={0}
-                      stepNumber={1}
-                      iconName={"material-symbols:timer"}
+          {isAuthenticated ? (
+            <Flex gap={{ base: "1.5rem" }} justify="center">
+              {currentStep.showSidebar && (
+                <VStack
+                  px="4"
+                  w="14rem"
+                  align="flex-start"
+                  divider={
+                    <Box
+                      h="2.25rem"
+                      w="1.42px"
+                      bg="gray.500"
+                      position="relative"
+                      left="15px"
                     />
-
-                    <Text
-                      whiteSpace="nowrap"
-                      fontWeight="medium"
-                      color={"primary"}
-                      fontSize="1.1rem"
+                  }
+                >
+                  {arrivalDateTime && (
+                    <Flex
+                      align="center"
+                      gap={{ base: "1.5rem", md: "1.82rem" }}
                     >
-                      {arrivalDateTime}
-                    </Text>
-                  </Flex>
-                )}
-                {steps.map((step) => {
-                  return (
-                    step.showInSidebar && (
-                      <Flex
-                        key={step.label}
-                        align="center"
-                        onClick={() => setCurrentStep(step)}
-                        cursor="pointer"
-                        gap={{ base: "1.5rem", md: "1.82rem" }}
+                      <ColorChangingCircle
+                        currentStepNumber={0}
+                        stepNumber={1}
+                        iconName={"material-symbols:timer"}
+                      />
+
+                      <Text
+                        whiteSpace="nowrap"
+                        fontWeight="medium"
+                        color={"primary"}
+                        fontSize="1.1rem"
                       >
-                        <ColorChangingCircle
-                          currentStepNumber={currentStep.stepNumber}
-                          stepNumber={step.stepNumber}
-                          iconName={step.iconName}
-                        />
-
-                        <Text
-                          whiteSpace="wrap"
-                          fontWeight="medium"
-                          color={
-                            currentStep.stepNumber === step.stepNumber
-                              ? "primary"
-                              : "gray.500"
-                          }
-                          fontSize="1.1rem"
+                        {arrivalDateTime}
+                      </Text>
+                    </Flex>
+                  )}
+                  {steps.map((step) => {
+                    return (
+                      step.showInSidebar && (
+                        <Flex
+                          key={step.label}
+                          align="center"
+                          onClick={() => setCurrentStep(step)}
+                          cursor="pointer"
+                          gap={{ base: "1.5rem", md: "1.82rem" }}
                         >
-                          {step.label}
-                        </Text>
-                      </Flex>
-                    )
-                  );
-                })}
-                {departureDateTime && (
-                  <Flex align="center" gap={{ base: "1.5rem", md: "1.82rem" }}>
-                    <ColorChangingCircle
-                      currentStepNumber={0}
-                      stepNumber={1}
-                      iconName={"material-symbols:timer"}
-                    />
+                          <ColorChangingCircle
+                            currentStepNumber={currentStep.stepNumber}
+                            stepNumber={step.stepNumber}
+                            iconName={step.iconName}
+                          />
 
-                    <Text
-                      whiteSpace="nowrap"
-                      fontWeight="medium"
-                      color={"primary"}
-                      fontSize="1.1rem"
+                          <Text
+                            whiteSpace="nowrap"
+                            fontWeight="medium"
+                            color={
+                              currentStep.stepNumber === step.stepNumber
+                                ? "primary"
+                                : "gray.500"
+                            }
+                            fontSize="1.1rem"
+                          >
+                            {step.label}
+                          </Text>
+                        </Flex>
+                      )
+                    );
+                  })}
+                  {departureDateTime && (
+                    <Flex
+                      align="center"
+                      gap={{ base: "1.5rem", md: "1.82rem" }}
                     >
-                      {departureDateTime}
-                    </Text>
-                  </Flex>
-                )}
-              </VStack>
-            )}
+                      <ColorChangingCircle
+                        currentStepNumber={0}
+                        stepNumber={1}
+                        iconName={"material-symbols:timer"}
+                      />
 
-            <Box flex="1">
-              <currentStep.component />
+                      <Text
+                        whiteSpace="nowrap"
+                        fontWeight="medium"
+                        color={"primary"}
+                        fontSize="1.1rem"
+                      >
+                        {departureDateTime}
+                      </Text>
+                    </Flex>
+                  )}
+                </VStack>
+              )}
+
+              <Box flex="1">
+                <currentStep.component />
+              </Box>
+            </Flex>
+          ) : (
+            <Box>
+              <Text
+                fontSize="2rem"
+                fontWeight="bold"
+                my="2rem"
+                textAlign="center"
+              >
+                Getting started to{" "}
+                <Text as="span" color="primary">
+                  Ghumante
+                </Text>
+              </Text>
+              <PhoneVerification redirectUrl="/" />
             </Box>
-          </Flex>
+          )}
         </ModalBody>
         {totalHours && (
           <ModalFooter>
