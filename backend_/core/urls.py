e29e4ac685451fa0b0187ff5d8813d015661b670
Neeraj Lamsal <itskop520@gmail.com>
2023-01-22 08:45:52 +0545

    add filtering by range on hotel and destinations

--- backend_/core/urls.py
+++ backend_/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CustomAuthToken
+from .views import CustomAuthToken, UserProfileAPIView
 
 urlpatterns = [
     path("login", CustomAuthToken.as_view(), name="login"),
+    path("user", UserProfileAPIView.as_view(), name="user_profile"),
 ]
--- backend_/core/views.py
+++ backend_/core/views.py
@@ -1,9 +1,15 @@
 # Create your views here.
 from core.models import User
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from core.serializers import UserResponseSerializer
 from firebase.client import FirebaseClient
 from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+firebase_client = FirebaseClient()
 
 
 class CustomAuthToken(ObtainAuthToken):
@@ -11,10 +17,18 @@ class CustomAuthToken(ObtainAuthToken):
         token = request.data.get("token", None)
         if token is None:
             return Response({"error": "Token is required"}, status=400)
-        firebase_client = FirebaseClient()
         user = firebase_client.verify_id_token(token)
         if not user:
             return Response({"error": "Invalid token"}, status=400)
         created_user = User.objects.get_or_create(phone_number=user["phone_number"])[0]
         token, _ = Token.objects.get_or_create(user=created_user)
         return Response({"token": token.key}, status=200)
+
+
+class UserProfileAPIView(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        serializer = UserResponseSerializer(request.user)
+        return Response(data=serializer.data, status=201)
